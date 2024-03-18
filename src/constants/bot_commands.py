619a6c5d49276938cb644491a7466ec5e2f406c5
Syrnnik <sakh.alexandr@gmail.com>
2024-03-18 23:05:45 +0300

    refactor: remove unused files

--- src/constants/bot_commands.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import BotCommand
-
-start_command = BotCommand(
-    command="start",
-    description="Start chat with bot"
-)
--- src/utils/bot/routers.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Dispatcher
-
-from handlers.start import start_router
-
-
-def set_bot_routers(dispatcher: Dispatcher):
-    dispatcher.include_routers(
-        start_router,
-    )
